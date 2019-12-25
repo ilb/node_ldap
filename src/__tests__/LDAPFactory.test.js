aa4c237e46ce0a2f47a251c251b97a5e7c5dea5d
slavb <slavb@ilb.ru>
2019-12-25 04:11:04 +0000

    LDAP_URL env support

--- src/__tests__/LDAPFactory.test.js
+++ src/__tests__/LDAPFactory.test.js
@@ -12,3 +12,17 @@ test('getInstance', async () => {
 
     ldapFactory.close();
 });
+
+process.env.LDAP_URL = 'ldaps://devel.net.ilb.ru/cc=ru';
+const ldapFactory2 = new LDAPFactory();
+
+test('getInstance', async () => {
+    const ldapResource = await ldapFactory2.getLDAPResource();
+    const resourceUrl = await ldapResource.lookup('ru.bystrobank.apps.testapp.db');
+
+    expect(resourceUrl).toBe(expected);
+
+    expect(ldapFactory2.ldapConfig.base).toBe('cc=ru');
+
+    ldapFactory2.close();
+});
